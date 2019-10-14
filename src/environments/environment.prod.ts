4357bf62b122031fc312ecbc5e8596b65d86ebc0
Max Baranowski <max.baranowski@mail.ru>
2019-10-14 13:36:46 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  FIREBASE_API_KEY: 'AIzaSyAvaRkF-0DA7gBNUbx00h4BY1E8x_IGhOI'
+  FIREBASE_API_KEY: 'AIzaSyAvaRkF-0DA7gBNUbx00h4BY1E8x_IGhOI',
+  FIREBASE_DB_URL: 'https://angular-blog-7e99f.firebaseio.com/'
 };
